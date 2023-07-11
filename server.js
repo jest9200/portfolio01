6557ad118d7012ef61cfe118d1ebcb2082e66bc6
정승혁 <jest9200@naver.com>
2023-07-11 10:08:23 +0900

    0711

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended: true}));
